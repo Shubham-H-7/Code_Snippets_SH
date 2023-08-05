72dd061954a17a890d02175fcc10227e393911d1
ShubhH7 <shubhamhiremath1996@gmail.com>
2023-08-05 21:35:01 +0530

    Names Gen

---
+++ src/main/java/org/example/NamesGen.java
@@ -0,0 +1,26 @@
+package org.example;
+import java.util.Random;
+public class NamesGen {
+
+        public static void main(String[] args) {
+            int N = 75; // Change this value to generate names N times
+
+            String[] europeanMaleNames = {
+                    "Andersen", "Bianchi", "Carlsen", "Dijkstra", "Eriksson", "Faber", "Gomes", "Hansen", "Ionescu", "Jansen",
+                    "Kovalenko", "Lehmann", "Muller", "Novak", "Donnell", "Petrović", "Rossi", "Schmidt", "Toth", "Van der Berg",
+                    /* Add more last names here */
+            };
+
+            Random random = new Random();
+
+            System.out.println("Generated European Male Names:");
+
+            for (int i = 0; i < N; i++) {
+                int randomIndex = random.nextInt(europeanMaleNames.length);
+                String name = europeanMaleNames[randomIndex];
+                System.out.println(name);
+            }
+        }
+    }
+
+
