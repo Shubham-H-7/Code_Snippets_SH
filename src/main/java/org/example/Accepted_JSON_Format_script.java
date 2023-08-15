9944b50cc735f744e7b25d524fd463f8b6d50190
ShubhH7 <shubhamhiremath1996@gmail.com>
2023-08-15 17:56:47 +0530

    Adding new changes, generated data to be sorted in file

--- src/main/java/org/example/Accepted_JSON_Format_script.java
+++ src/main/java/org/example/Accepted_JSON_Format_script.java
@@ -1,66 +1,64 @@
 package org.example;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+
+import java.io.*;
+
 public class Accepted_JSON_Format_script {
+    public static void main(String[] args) {
+        String inputFilePath = "/Users/shubham/Downloads/Array_Gen_Data_File.json";   // Replace with your input JSON file path
+        String outputFilePath = "/Users/shubham/Downloads/Single_line_convert.json"; // Replace with your output JSON file path
 
-        public static void main(String[] args) {
-            String inputFilePath = "/Users/shubham/Downloads/new_array_data.json";   // Replace with your input JSON file path
-            String outputFilePath = "/Users/shubham/Downloads/final_array_data.json"; // Replace with your output JSON file path
-
-            try {
-                String jsonString = readJSONStringFromFile(inputFilePath);
-                String singleLineJSON = jsonString.replaceAll("\\s+", "");
-                writeJSONToFile(singleLineJSON, outputFilePath);
-                System.out.println("Conversion complete.");
-                formatJSON(outputFilePath);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        private static String readJSONStringFromFile(String filePath) throws IOException {
-            try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-                StringBuilder jsonString = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    jsonString.append(line);
-                }
-                return jsonString.toString();
-            }
-        }
-        private static void writeJSONToFile(String jsonContent, String filePath) throws IOException {
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-                writer.write(jsonContent);
-            }
-        }
-
-        private static void formatJSON(String outputFilePath){
-            try {
-                BufferedReader reader = new BufferedReader(new FileReader(outputFilePath));
-                BufferedWriter writer = new BufferedWriter(new FileWriter("/Users/shubham/Downloads/Array_data_new.json"));
-                String line;
-                try {
-                    line = reader.readLine();
-                    while (line != null) {
-                        String[] objects = line.split("(?<=\\})|(?=\\{)");
-                        for (String object : objects) {
-                            writer.write(object.trim());
-                            writer.newLine();
-                        }
-                        line = reader.readLine();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                reader.close();
-                writer.close();
-                System.out.println("JSON objects separated and written to output.json");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        try {
+            String jsonString = readJSONStringFromFile(inputFilePath);
+            String singleLineJSON = jsonString.replaceAll("\\s+", "");
+            writeJSONToFile(singleLineJSON, outputFilePath);
+            System.out.println("Conversion complete.");
+            formatJSON(outputFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+
+    private static String readJSONStringFromFile(String filePath) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            StringBuilder jsonString = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                jsonString.append(line);
+            }
+            return jsonString.toString();
+        }
+    }
+
+    private static void writeJSONToFile(String jsonContent, String filePath) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            writer.write(jsonContent);
+        }
+    }
+
+    private static void formatJSON(String outputFilePath) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(outputFilePath));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("/Users/shubham/Downloads/Final_Array_Data.json"));
+            String line;
+            try {
+                line = reader.readLine();
+                while (line != null) {
+                    String[] objects = line.split("(?<=\\})|(?=\\{)");
+                    for (String object : objects) {
+                        writer.write(object.trim());
+                        writer.newLine();
+                    }
+                    line = reader.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            reader.close();
+            writer.close();
+            System.out.println("JSON objects separated and written to output.json");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
 
--- src/main/java/org/example/Array_Data_Gen.java
+++ src/main/java/org/example/Array_Data_Gen.java
@@ -1,123 +1,114 @@
 package org.example;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import org.apache.commons.csv.*;
+import org.json.*;
 import java.io.*;
-
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Array_Data_Gen {
 
     // This code helps to generate array data type data for testing
-    static  int i;
-        public static void main(String[] args) {
+    static int i;
 
+    public static void main(String[] args) throws IOException {
+        JSONArray jsonArray = new JSONArray();
 
-
-            JSONArray jsonArray = new JSONArray();
-
-            //Here please copy and paste the IDs file path from which you want IDs to be fetched and be part of Array data
-            List<String> adIds = readAdIdsFromCSV("/Users/shubham/Downloads/1000_AdId_IDs.csv");
-            if (adIds.isEmpty()) {
-                System.err.println("No AdIds found in the CSV file.");
-                return;
-            }
-            for (i = 1; i < 100; i++)
-            {
-                JSONObject jsonObject = createSampleJsonObject(adIds);
-                jsonArray.put(jsonObject);
-            }
-
-            System.out.println(jsonArray.toString(4)); // Printing JSON with indentation of 4 spaces
+        //Here please copy and paste the IDs file path from which you want IDs to be fetched and be part of Array data
+        List<String> adIds = readAdIdsFromCSV("/Users/shubham/Downloads/1000_AdId_IDs.csv");
+        if (adIds.isEmpty()) {
+            System.err.println("No AdIds found in the CSV file.");
+            return;
+        }
+        for (i = 1; i < 5; i++) {
+            JSONObject jsonObject = createSampleJsonObject(adIds);
+            jsonArray.put(jsonObject);
         }
 
-
-        private static JSONObject createSampleJsonObject(List<String> adIds) {
-            JSONObject jsonObject = new JSONObject();
-
-            int j;
-            for (j=0; j<i; j++) {
-                String adId = adIds.get(j);
-                jsonObject.put("ID", adId);
-            }
-                // Generate random age between 20 and 30 (inclusive)
-                int age = generateRandomAge(30, 35);
-                jsonObject.put("age", age);
-                jsonObject.put("isConsent", generateRandomYesNo());
-                jsonObject.put("isMarketingPreference", generateRandomYesNo() );
-
-                // Add an array of values (Transcations) [Data Type - Double]
-                JSONArray transcations = new JSONArray();
-                transcations.put(95000.00);
-                transcations.put(97000.01);
-                transcations.put(99000.90);
-                transcations.put(93000.45);
-                transcations.put(94000);
-                jsonObject.put("Transcations", transcations);
-
-                // Add an array of values (Banks) [Data Type - String]
-                JSONArray banks = new JSONArray();
-                banks.put("JPMorgan");
-                banks.put("BAC");
-                banks.put("Wells Fargo");
-                jsonObject.put("Banks", banks);
-
-                // Add an array of values (Credit) [Data Type - Integer]
-                JSONArray credit = new JSONArray();
-                credit.put(9000);
-                credit.put(9500);
-                credit.put(9999);
-                credit.put(94500);
-
-                jsonObject.put("credited", credit);
-
-            long timestamp = generateRandomTimestamp(1688196888, 1690702488); // Example date range: 11 August 2023 to  21 August 2023
-            jsonObject.put("timestamp", timestamp);
-
-            return jsonObject;
+        String formattedJsonArray = jsonArray.toString(4); // Storing JSON with indentation of 4 spaces
+        String filepath = "/Users/shubham/Downloads/Array_Gen_Data_File.json";
+        try (BufferedWriter wrt= new BufferedWriter(new FileWriter(filepath))){
+            wrt.write(formattedJsonArray);
         }
-
-        private static int generateRandomAge(int min, int max) {
-            Random random = new Random();
-            return random.nextInt(max - min + 1) + min;
+        catch (Exception e1){
+            e1.printStackTrace();
         }
+    }
 
-        private static boolean getRandomBoolean() {
-            Random random = new Random();
-            return random.nextBoolean();
+    private static JSONObject createSampleJsonObject(List<String> adIds) {
+        JSONObject jsonObject = new JSONObject();
+
+        int j;
+        for (j = 0; j < i; j++) {
+            String adId = adIds.get(j);
+            jsonObject.put("ID", adId);
         }
-        private static String generateRandomYesNo() {
+        // Generate random age between 20 and 30 (inclusive)
+        int age = generateRandomAge(30, 35);
+        jsonObject.put("age", age);
+        jsonObject.put("isConsent", generateRandomYesNo());
+        jsonObject.put("isMarketingPreference", generateRandomYesNo());
+
+        // Add an array of values (Transcations) [Data Type - Double]
+        JSONArray transcations = new JSONArray();
+        transcations.put(95000.00);
+        transcations.put(97000.01);
+        jsonObject.put("Transcations", transcations);
+
+        // Add an array of values (Banks) [Data Type - String]
+        JSONArray banks = new JSONArray();
+        banks.put("JPMorgan");
+        banks.put("Wells Fargo");
+        jsonObject.put("Banks", banks);
+
+        // Add an array of values (Credit) [Data Type - Integer]
+        JSONArray credit = new JSONArray();
+        credit.put(9000);
+        credit.put(9500);
+
+        jsonObject.put("credited", credit);
+
+        long timestamp = generateRandomTimestamp(1688196888, 1690702488); // Example date range: 11 August 2023 to  21 August 2023
+        jsonObject.put("timestamp", timestamp);
+
+        return jsonObject;
+    }
+
+    private static int generateRandomAge(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min + 1) + min;
+    }
+
+    private static boolean getRandomBoolean() {
+        Random random = new Random();
+        return random.nextBoolean();
+    }
+
+    private static String generateRandomYesNo() {
         String[] options = {"yes", "no"};
         Random random = new Random();
         int randomIndex = random.nextInt(options.length);
         return options[randomIndex];
     }
+
     private static long generateRandomTimestamp(long min, long max) {
         Random random = new Random();
         long randomTimestamp = random.nextLong() % (max - min) + min;
         return randomTimestamp;
     }
 
-        private static List<String> readAdIdsFromCSV(String filePath) {
-            List<String> adIds = new ArrayList<>();
+    private static List<String> readAdIdsFromCSV(String filePath) {
+        List<String> adIds = new ArrayList<>();
 
-            try {
-                CSVParser parser = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(new FileReader(filePath));
+        try {
+            CSVParser parser = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(new FileReader(filePath));
 
-                for (CSVRecord record : parser) {
-                    String adId = record.get("AdId"); // Change "AdId" to the column header for IDs
-                    adIds.add(adId);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+            for (CSVRecord record : parser) {
+                String adId = record.get("AdId"); // Change "AdId" to the column header for IDs
+                adIds.add(adId);
             }
-
-            return adIds;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+        return adIds;
     }
+}
